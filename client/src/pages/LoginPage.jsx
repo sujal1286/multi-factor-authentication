46cd951ea24d15af4abefc10becf204e241536cd
SUJAL-GOHEL <144356627+sujal1286@users.noreply.github.com>
2025-11-10 15:12:20 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm.jsx';
-
+import React from "react";
+import LoginForm from "../components/LoginForm.jsx";
+import Home from "./Home.jsx";
 const LoginPage = () => {
-  return <LoginForm />
-}
+  return <LoginForm />;
+};
 
 export default LoginPage;
